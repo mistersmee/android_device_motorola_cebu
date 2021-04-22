a0fb43e3e83039f1a74e66713413e405670fa1ad
Aseem Athale <athaleaseem@gmail.com>
2021-04-22 12:30:23 +0530

    cebu: re-add accidentally deleted copyright stuff.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 #
-# Copyright 2021 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

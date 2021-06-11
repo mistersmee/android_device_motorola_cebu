6de87c7e5753f367db102dd85d893ecde5ba487c
Aseem Athale <athaleaseem@gmail.com>
2021-06-11 11:42:06 +0530

    cebu: switch to building from kernel sources
    instead of using the prebuilt kernel and dtb, dtbo images, build them
      from source.
    
    Change-Id: Ie656a1925a6e151b1d0e3169d76c880918e0985f

--- recovery/root/system/bin/load_ts_firmware.sh
+++ recovery/root/system/bin/load_ts_firmware.sh
@@ -31,21 +31,21 @@ wait_for_poweron()
 }
 
 # Load all needed modules
-insmod $module_path/sensors_class.ko
-insmod $module_path/fpc1020_mmi.ko
-insmod $module_path/utags.ko
-insmod $module_path/exfat.ko
-insmod $module_path/mmi_annotate.ko
-insmod $module_path/mmi_info.ko
-insmod $module_path/mmi_sys_temp.ko
-insmod $module_path/moto_f_usbnet.ko
-insmod $module_path/qpnp-power-on-mmi.ko
-insmod $module_path/qpnp_adaptive_charge.ko
+#insmod $module_path/sensors_class.ko
+#insmod $module_path/fpc1020_mmi.ko
+#insmod $module_path/utags.ko
+#insmod $module_path/exfat.ko
+#insmod $module_path/mmi_annotate.ko
+#insmod $module_path/mmi_info.ko
+#insmod $module_path/mmi_sys_temp.ko
+#insmod $module_path/moto_f_usbnet.ko
+#insmod $module_path/qpnp-power-on-mmi.ko
+#insmod $module_path/qpnp_adaptive_charge.ko
 insmod $module_path/focaltech_0flash_mmi.ko
 insmod $module_path/nova_0flash_mmi.ko
 insmod $module_path/chipone_tddi_mmi.ko
-insmod $module_path/leds_aw99703.ko
-insmod $module_path/ktd3136_bl.ko
+#insmod $module_path/leds_aw99703.ko
+#insmod $module_path/ktd3136_bl.ko
 
 cd $firmware_path
 touch_product_string=$(ls $touch_class_path)
